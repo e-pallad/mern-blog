c5699c8aca79a9e2275956148ba5c4280c2faf4c
e-pallad <eric.pallad@devfluent.de>
2024-12-06 12:48:37 +0100

    add create post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,27 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "Not allowed to create a post"));
+  }
+  if (!req.user.title || !req.user.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .joint("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
