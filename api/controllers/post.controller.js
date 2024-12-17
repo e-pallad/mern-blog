9478d3acb28aae1a38cc5d21c63a7d1e4b221ea5
e-pallad <eric.pallad@devfluent.de>
2024-12-17 11:32:16 +0100

    complete post function

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,14 +3,14 @@ import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, "Not allowed to create a post"));
+    return next(errorHandler(403, "You are not allowed to create a post"));
   }
-  if (!req.user.title || !req.user.content) {
+  if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, "Please provide all required fields"));
   }
   const slug = req.body.title
     .split(" ")
-    .joint("-")
+    .join("-")
     .toLowerCase()
     .replace(/[^a-zA-Z0-9-]/g, "");
   const newPost = new Post({
