4131fa01ceb1c3f2ad193e1fac18613ec32756c9
e-pallad <eric.pallad@devfluent.de>
2025-01-07 14:06:42 +0100

    add delete user func to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -50,17 +50,15 @@ export default function DashUsers() {
   const handleDeleteUser = async () => {
     setShowModal(false);
     try {
-      const res = await fetch(
-        `/api/user/deleteuser/${postIdToDelete}/${currentUser._id}`,
-        { method: "DELETE" }
-      );
+      const res = await fetch(`/api/user/deleteuser/${userIdToDelete}`, {
+        method: "DELETE",
+      });
       const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowModal(false);
       } else {
-        setUserPosts((prev) =>
-          prev.filter((post) => post._id !== postIdToDelete)
-        );
+        console.log(data.message);
       }
     } catch (error) {
       console.log(error.message);
