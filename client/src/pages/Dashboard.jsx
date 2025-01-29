02f50519f5cd67376d45ca3a2ab92381c73129e5
e-pallad <eric.pallad@devfluent.de>
2025-01-29 12:33:58 +0100

    complete dashboard section of admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -29,6 +30,7 @@ export default function Dashboard() {
       {tab === "posts" && <DashPosts />}
       {tab === "comments" && <DashComments />}
       {tab === "users" && <DashUsers />}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 }
